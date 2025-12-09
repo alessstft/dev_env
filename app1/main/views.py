6d067345356f37e6a5834283763ea63621612f0c
aless <aless@LAPTOP-4JA9E7JL>
2025-12-09 03:39:13 +0300

    уффффф

--- app1/main/views.py
+++ app1/main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context: dict = {
         "title" : "FreeCloth",
-        "content":"Магазин одежды FreeCloth"
+        "content":"Магазин одежды FreeCloth",
+        'categories' : categories
     }
     return render(request, 'main/index.html', context)
  
