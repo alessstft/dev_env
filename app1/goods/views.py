53becd1472e5769522b125f5732e1ce4cd5c1f63
aless <aless@LAPTOP-4JA9E7JL>
2025-12-10 21:00:33 +0300

    нажатие на карточку

--- app1/goods/views.py
+++ app1/goods/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 
 from goods.models import Products
@@ -14,5 +15,13 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_id):
+
+    product = Products.objects.get(id=product_id)
+    
+    product_item = get_object_or_404(Products, id=product_id)
+    
+    context = {
+        'product': product,
+    }
+    return render(request, 'goods/product.html', context=context)
